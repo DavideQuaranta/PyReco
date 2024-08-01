deb9611cd1be7db02169f12066a1cbab451c93a4
DavideQuaranta <quaranta.1895475@studenti.uniroma1.it>
2024-08-01 10:10:07 +0200

    removed everything

--- PyReco/rootio.py
+++
@@ -1,6 +0,0 @@
-import uproot as root
-
-def open_tree(filename: str, treename: str):
-    file = root.open(filename)
-    print(f"- \"{treename}\" from {filename} opened with success...")
-    return file[treename]
